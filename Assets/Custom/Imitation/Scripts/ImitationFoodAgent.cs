98f5fe55ac03c868c96906147f97e3272c094a28
Kishor Deshmukh <kishor.p.deshmukh@gmail.com>
2024-01-09 22:01:46 -0400

    Food agent script more content

--- Assets/Custom/Imitation/Scripts/ImitationFoodAgent.cs
+++ Assets/Custom/Imitation/Scripts/ImitationFoodAgent.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 using UnityEngine;
 
 public class ImitationFoodAgent : Agent
@@ -23,5 +25,84 @@ public class ImitationFoodAgent : Agent
     public override void OnEpisodeBegin()
     {
         transform.localPosition = Vector3.zero;
+
+        OnEpisodeBeginEvent?.Invoke(this, EventArgs.Empty);
     }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(rewardButton.CanUseButton() ? 1 : 0);
+
+        Vector3 dirToFoodButton = (rewardButton.transform.localPosition - transform.localPosition).normalized;
+        sensor.AddObservation(dirToFoodButton.x);
+        sensor.AddObservation(dirToFoodButton.z);
+
+        sensor.AddObservation(foodSpawner.HasFoodSpawned() ? 1 : 0);
+
+        if (foodSpawner.HasFoodSpawned())
+        {
+            Vector3 dirToFood = (foodSpawner.GetLastFoodTransform().localPosition - transform.localPosition).normalize;
+        sensor.AddObservation(dirToFood.x);
+            sensor.AddObservation(dirToFood.z);
+        }
+        else
+        {
+            // Food not spawned
+            sensor.AddObservation(0f); // x
+            sensor.AddObservation(0f); // z
+                                       // }
+        }
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        int moveX = actions.DiscreteActions[0]; // 0 = Dont Move; 1 = Left; 2 = Right
+        int moveZ = actions.DiscreteActions[1]; // 0 = Dont Move; 1 = Back; 2 = Forward
+
+        Vector3 addForce = new Vector3(0, 0, 0);
+
+        switch (moveX)
+        {
+            case 0: addForce.x = 0f; break;
+            case 1: addForce.x = -1f; break;
+            case 2:
+                addForce.x = +1f; break;
+        }
+        switch (moveZ)
+        {
+            case 0: addForce.z = 0f; break;
+            case 1: addForce.z = -1f; break;
+            case 2: addForce.z = +1f; break;
+        }
+
+
+        float moveSpeed = 5f;
+        rbAgent.velocity = addForce * moveSpeed + new Vector3(0, rbAgent.velocity.y, 0);
+
+        bool isUseButtonDown = actions.DiscreteActions[2] == 1;
+        if (isUseButtonDown)
+        {
+            // Use Action
+            Collider[] colliderArray = Physics.OverlapBox(transform.position, Vector3.one * .5f);
+            foreach (Collider collider in colliderArray)
+            {
+                if (collider.TryGetComponent<RewardButton>(out RewardButton foodButton))
+                {
+                    if (foodButton.CanUseButton())
+                    {
+                        foodButton.UseButton();
+                        AddReward(1f);
+
+                    }
+
+                }
+            }
+        }
+
+
+    }
+
+
+
+
 }
