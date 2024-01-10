100b385305159cdfe049303b8aefd24fffbe7b4e
Kishor Deshmukh <kishor.p.deshmukh@gmail.com>
2024-01-09 22:01:56 -0400

    Reward button script

--- Assets/Custom/Imitation/Scripts/RewardButton.cs
+++ Assets/Custom/Imitation/Scripts/RewardButton.cs
@@ -1,18 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RewardButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public event EventHandler OnUsed;
+
+    [SerializeField] private Material greenMaterial;
+    [SerializeField] private Material greenDarkMaterial;
+
+    private MeshRenderer buttonMeshRenderer;
+    private Transform buttonTransform;
+    private bool canUseButton;
+
+    private void Awake()
     {
-        
+        buttonTransform = transform.Find("Button");
+        buttonMeshRenderer = buttonTransform.GetComponent<MeshRenderer>();
+        canUseButton = true;
     }
+    
+
+    private void Start()
+    {
+        ResetButton();
+
+    }
+    public bool CanUseButton()
+    {
+        return canUseButton;
+
+    }
+
+    public void UseButton()
+    {
+        if (canUseButton)
+        {
+            buttonMeshRenderer.material = greenDarkMaterial;
+            buttonTransform.localScale = new Vector3(.5f, .2f, .5f);
+            canUseButton = false;
+
+            OnUsed?.Invoke(this, EventArgs.Empty);
+
+        }
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void ResetButton()
+    {
+        buttonMeshRenderer.material = greenMaterial;
+        buttonTransform.localScale = new Vector3(.5f, .5f, .5f);
+
+        transform.localPosition =
+        new Vector3(
+        transform.localPosition.x, transform.localPosition.y, 3);
+
+        canUseButton = true;
+
+    }
+
 }
