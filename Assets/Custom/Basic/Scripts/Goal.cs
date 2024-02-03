ea396a67e8e5b968332eeda9212d14b79edbd786
Kishor Deshmukh <kishor.p.deshmukh@gmail.com>
2024-02-03 06:57:18 -0400

    Update the goal script to have more debug data

--- Assets/Custom/Basic/Scripts/Goal.cs
+++ Assets/Custom/Basic/Scripts/Goal.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    
+    [SerializeField] private Material NoneMat;
+    [SerializeField] private Material WinMat;
+    [SerializeField] private Material LoseMat;
+
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    public enum GoalState
     {
-        
+        None,
+        Win,
+        Lose
     }
 
-    // Update is called once per frame
-    void Update()
+    public GoalState currentGoalState;
+
+
+    public void Awake()
     {
-        
+        Default();
+    }
+
+    public void Won()
+    {
+        currentGoalState = GoalState.Win;
+        UpdateMaterial();
+    }
+    
+    public void Lost()
+    {
+
+        currentGoalState = GoalState.Lose;
+        UpdateMaterial();
+    }
+
+    
+    public void Default()
+    {
+
+        currentGoalState = GoalState.None;
+        UpdateMaterial();
+    }
+
+
+
+
+    void UpdateMaterial()
+    {
+        switch (currentGoalState)
+        {
+            case GoalState.None : meshRenderer.material = NoneMat;   break;
+            case GoalState.Win  : meshRenderer.material = WinMat;    break;
+            case GoalState.Lose : meshRenderer.material = LoseMat;   break;
+        }
     }
 }
