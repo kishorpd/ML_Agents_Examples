a8842a7bd5b5972711acc76a504132ff3d42e6c0
Kishor Deshmukh <kishor.p.deshmukh@gmail.com>
2024-02-03 19:33:34 -0400

    Reset the First example to the master branch state

--- Assets/Custom/Basic/Scripts/GoalAgent3DForce.cs
+++ Assets/Custom/Basic/Scripts/GoalAgent3DForce.cs
@@ -5,7 +5,6 @@ using Unity.Mathematics;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Android;
 using Random = UnityEngine.Random;
@@ -23,7 +22,6 @@ public class GoalAgent3DForce : Agent
     public GameObject rootX;
     public GameObject rootY;
     public GameObject rootZ;
-    public GameObject rootTorqY;
     private Rigidbody rb;
     public GameObject FuelIndicator;
     public float DefaultFuelSize = 100;
@@ -39,139 +37,22 @@ public class GoalAgent3DForce : Agent
     public ParticleSystem posBoosterZ;
     public ParticleSystem negBoosterZ;
 
-    Vector3 startingPosition;
-    Quaternion startingRotation;
-
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         InitBoosters();
-        startingPosition = transform.localPosition;
-        startingRotation = transform.localRotation;
-
-        previousAngle = startingRotation.eulerAngles.y;
-
-        ChangeRewardPosition();
-
-   //     targetTransform.localPosition = new Vector3(Random.Range(-rangeH,rangeH), 0, -rangeV);
     }
     
     float forceX;
     float forceY;
     float forceZ;
-    float torqueY;
     public float debugForceMultiplier = 3;
-
-
-
-    public float penaltyDistanceDifference = 0.7f;
-
- //   float minDistance = 999;
-    public float currentDistance = 0;
-
-    public bool bInRewardsRange = false;
-
 
     void FixedUpdate()
     {
         ForceApplication();
-        CheckAngle();
-
-        currentDistance = Vector3.Distance(transform.localPosition, targetTransform.localPosition);
-
-        /*
-        if (minDistance > currentDistance) { minDistance = currentDistance; }
-
-        if (!bInRewardsRange) bInRewardsRange = minDistance < penaltyDistanceDifference;
-
-        if (bInRewardsRange)
-        {
-            if (currentDistance > penaltyDistanceDifference)
-                Penalize();
-        }
-         */
     }
-
-
-    float totalAngleDifference = 0;
-    float previousAngle = 0;
-
-
-
-    void CheckAngle()
-    {
-        float angleDifference = transform.localEulerAngles.y - startingRotation.eulerAngles.y;
-
-        float deltaAngle = transform.localEulerAngles.y - previousAngle;
-
-        if(math.abs(deltaAngle) < 5) totalAngleDifference += deltaAngle;
-
-        previousAngle = transform.localEulerAngles.y;
-
-        //
-        //        float clampedRotation = Mathf.Repeat(angleDifference, 360f);
-        //
-        //        if (clampedRotation < 0)
-        //        {
-        //            clampedRotation += 360f;
-        //        }
-        //
-
-        if (math.abs(totalAngleDifference) > 359) Penalize(); 
-
-        /*
-        //For gizmo
-        Handles.Label(
-            transform.localPosition, 
-            " angleDifference : " + angleDifference +
-            "\n clampedRotation : " + clampedRotation +
-            "\n deltaAngle : " + deltaAngle +
-            "\n totalAngleDifference : " + totalAngleDifference + 
-            "\n ABS totalAngleDifference : " + math.abs(totalAngleDifference) + 
-            " \n transform.rotation.eulerAngles.z : " + transform.rotation.eulerAngles.y
-            );
-        */
-    }
-
-
-    public float angleBetween = 0;
-
-   // float rewardValue = 0;
-
-    private void OnDrawGizmos()
-    {
-
-        //Handles.Label(transform.localPosition, "angle : " + getTargetAngle() + " IsTargetInFront : " + IsTargetInFront(transform,targetTransform));
-        /*
-        if (bChangeRewardPos)
-        {
-            bChangeRewardPos = false;
-            targetTransform.localPosition = new Vector3(-5,0,Random.Range(-5, 1.5f));
-        }
-        */
-    }
-
-    float getTargetAngle()
-    {
-        Vector3 LookDirection = transform.forward;
-        Vector3 relativeDirection = (targetTransform.position - transform.position).normalized;
-        return 135 - Vector3.Angle(LookDirection, relativeDirection);
-    }
-
-
-    private bool IsTargetInFront(Transform playerT, Transform targetT)
-    {
-        // Get the vector from the car to the waypoint
-        Vector3 toWaypoint = targetT.position - playerT.position;
-
-        // Calculate the dot product of the car's forward vector and the vector to the waypoint
-        float dotProduct = Vector3.Dot(-playerT.right, toWaypoint);
-
-
-        // If the dot product is negative, the waypoint is on the back side of the car
-        return dotProduct < 0f;
-    }
-
+    
     void KeyBased()
     {
         forceX = Input.GetAxis("Horizontal");
@@ -180,25 +61,15 @@ public class GoalAgent3DForce : Agent
         ForceApplication();
     }
 
-    public RocketBooster rRocketBooster;
-    public RocketBooster lRocketBooster;
-
     void ForceApplication()
     {
         rb.AddForce(CalculateForce(135, forceX*forceMagnitude), ForceMode.Impulse);
         rb.AddForce( new Vector3(0, forceY * forceMagnitude,0), ForceMode.Impulse);
         rb.AddForce(CalculateForce(45, forceZ*forceMagnitude), ForceMode.Impulse);
 
-        
-
-        if(torqueY > 0) { rRocketBooster.AddForceOrTorque(math.abs(torqueY)); }
-        else { lRocketBooster.AddForceOrTorque(math.abs(torqueY)); }
-
         rootX.transform.localScale = new Vector3(1,-forceX,1);
         rootY.transform.localScale = new Vector3(1,-forceY,1);
         rootZ.transform.localScale = new Vector3(1,-forceZ,1);
-
-        rootTorqY.transform.localScale = new Vector3(1,-torqueY,1);
 
         FuelCalculations();
         UpdateBoosters();
@@ -260,18 +131,15 @@ public class GoalAgent3DForce : Agent
     }
 
     public float FuelRemaining = 100;
-
-
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition);//3
-
+        sensor.AddObservation(targetTransform.localPosition);//3
+        var direction = (transform.localPosition - targetTransform.localPosition).normalized;
+        sensor.AddObservation(direction);//3
+        sensor.AddObservation(Vector3.Distance(transform.localPosition,targetTransform.localPosition));//3
         sensor.AddObservation(FuelRemaining);//1
-        sensor.AddObservation(getTargetAngle());//1
-
-        sensor.AddObservation(IsTargetInFront(transform,targetTransform) );//1
-        //sensor.AddObservation(totalAngleDifference);//1
-        //Total : 6
+        //Total : 13
     }
 
 
@@ -281,7 +149,6 @@ public class GoalAgent3DForce : Agent
         continuousActions[0] = -Input.GetAxis("Horizontal");
         continuousActions[1] = Input.GetAxis("Vertical");
         continuousActions[2] = (Input.GetKey(KeyCode.I) ? 1 : 0) + (Input.GetKey(KeyCode.K) ? -1 : 0);
-        continuousActions[3] = (Input.GetKey(KeyCode.J) ? 1 : 0) + (Input.GetKey(KeyCode.L) ? -1 : 0);
     }
 
     public int EmmisionRateForParticles = 80;
@@ -306,7 +173,6 @@ public class GoalAgent3DForce : Agent
         forceX = -actions.ContinuousActions[0];
         forceZ = actions.ContinuousActions[1];
         forceY = actions.ContinuousActions[2];
-        torqueY = actions.ContinuousActions[3];
     }
 
     void ResetTimer()
@@ -314,53 +180,21 @@ public class GoalAgent3DForce : Agent
         FuelRemaining = DefaultFuelSize;
     }
 
-    void ResetParams()
-    { 
-        totalAngleDifference = 0;
-   //     minDistance = 999;
-        bInRewardsRange = false;
-    }
-
     private void ResetScene()
     {
-        ResetParams();
         ResetTimer();
-        //transform.localPosition = new Vector3(0,1,0);
-
-
-        transform.localPosition = startingPosition;
-        transform.localRotation = startingRotation;
-
+        transform.localPosition = new Vector3(0,1,0);
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
-        rb.freezeRotation = true;
-        Invoke("EnableRotation", .1f);
     }
 
-    void EnableRotation()
-    {
-        rb.freezeRotation = false;
-    }
 
     public float rangeH = 1;
-    public float rangeReward = 1;
     public float rangeV = 1;
-
-    public bool bChangeRewardPos = true;
-
-    public bool forth = false;
-
     void ChangeRewardPosition()
     {
 
-        //targetTransform.localPosition = new Vector3(Random.Range(-7,7), 3, Random.Range(-5,2f));
-        targetTransform.localPosition = new Vector3(Random.Range(-6, 8), 1, Random.Range(-4,1f));
-        //targetTransform.localPosition = new Vector3(Random.Range(-rangeReward, rangeReward), 1, Random.Range(-4,1f));
-
-      //  targetTransform.localPosition = new Vector3(-rangeReward, 1, Random.Range(-5, 1.5f));
-  //      targetTransform.localPosition = new Vector3(Random.Range(rangeReward, -rangeReward) * (forth ? 1 : -1), 1, Random.Range(-5, 1.5f));
-      //  forth = !forth;
-        //targetTransform.localPosition = new Vector3(Random.Range(-7,7), Random.Range(1, 4f), Random.Range(-5,2f));
+        targetTransform.localPosition = new Vector3(Random.Range(-7,7), Random.Range(1, 4f), Random.Range(-5,2f));
     }
     
     void ChangeRewardPositionRadial()
@@ -401,23 +235,15 @@ public class GoalAgent3DForce : Agent
         ResetScene();
     }
 
-    float maxAngle = 10;
-
     void GiveRewards(float reward = 1f)
     {
-        float currentAngle = math.abs(getTargetAngle());
-        float rewardValue = 
-            ((currentAngle <= maxAngle) && IsTargetInFront(transform, targetTransform)) ? 
-            (1 - (currentAngle/ maxAngle)) : 0;
-
-        SetReward(rewardValue);
+        SetReward(1f);
         FloorMeshRenderer.material = winMaterial;
         
         //Change position only when it succeeds
         ChangeRewardPosition();
 
         EndEpisode();
-        ResetParams();
-       // ResetScene();
+      //  ResetScene();
     }
 }
