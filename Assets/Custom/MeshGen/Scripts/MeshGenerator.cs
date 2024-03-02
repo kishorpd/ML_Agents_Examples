5af899a59097ddc0859be059b01a2f51c38b954e
Kishor Deshmukh <kishor.p.deshmukh@gmail.com>
2024-03-01 21:51:01 -0400

    Update MeshGenerator.cs

--- Assets/Custom/MeshGen/Scripts/MeshGenerator.cs
+++ Assets/Custom/MeshGen/Scripts/MeshGenerator.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+  
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    // Use this for initialization
     void Start()
     {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void CreateShape()
     {
-        
+
+        vertices = new Vector3[]
+            {
+
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0)
+
+            };
+
+        triangles = new int[]
+        {
+            0, 1, 2
+
+        };
+
+
+    }
+
+    void UpdateMesh()
+    { 
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
     }
 }
